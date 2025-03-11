5c65f9a60f105925c251409c79a4dc287e0ea2be
Fábio Costa <a22997@alunos.ipca.pt>
2025-03-11 10:45:38 +0000

    bananinha123

---
+++ backend/backend/data/personD.cs
@@ -0,0 +1,15 @@
+﻿using models;
+
+namespace data
+{
+    public class personD
+    {
+        static List<Person> people = new List<Person>(5);
+
+        public static bool AddPerson(Person p)
+        {
+            people.Add(p);
+            return true;
+        }   
+    }
+}
---
+++ backend/backend/logicanegocios/personL.cs
@@ -0,0 +1,15 @@
+﻿using data;
+
+namespace logicanegocios
+{
+    public class personL
+    {
+        public static bool AddPerson(models.Person p)
+        {
+            if (p.Id == null)
+                return false;
+
+            return personD.AddPerson(p);
+        }
+    }
+}
