f43e88a79865f860a70dd273bb18f109a6688276
AnyTng <goncalo.tierri@gmail.com>
2025-03-26 10:31:39 +0000

    gitignore

--- backend/RESTful API/obj/Debug/net9.0/RESTful API.AssemblyInfo.cs
+++ backend/RESTful API/obj/Debug/net9.0/RESTful API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTful API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a0900336a7b76e0701346bc57f33f9706d7028")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319e8b10fa6dbcde83b1ddde07feb1ab0963ac08")]
 [assembly: System.Reflection.AssemblyProductAttribute("RESTful API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RESTful API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
