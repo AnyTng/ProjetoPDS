fe6472059226437a6275627f0d6c4d5ded4173e4
AnyTng <goncalo.tierri@gmail.com>
2025-04-24 19:23:28 +0100

    CUNTISSIMO-less :(

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -36,19 +36,6 @@ const Footer = () => {
                 </div>
 
 
-                <iframe
-                    allow="autoplay *; encrypted-media *;"
-                    frameBorder="0"
-                    height="150"
-                    style={{
-                        width: "100%",
-                        maxWidth: "660px",
-                        overflow: "hidden",
-                        background: "transparent"
-                    }}
-                    sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-                    src="https://embed.music.apple.com/pt/album/cuntissimo/1807308262?i=1807308322"
-                ></iframe>
             </div>
         </footer>
     );
