f352224583df6cb4710b6ce63c8066d5e9be946d
Fábio Costa <132665477+fabiocosta191@users.noreply.github.com>
2025-04-29 17:32:37 +0100

    a
    a

--- backend/RESTful API/Service/ServicoInterno.cs
+++ backend/RESTful API/Service/ServicoInterno.cs
@@ -20,20 +20,56 @@ namespace RESTful_API.Service
         
         private readonly ILogger<ServicoInterno> _logger;
         private readonly IEmailService _emailService;
+        private readonly IConfiguration _config;
+        private readonly PdsContext _context;
 
 
-        public ServicoInterno(ILogger<ServicoInterno> logger, IEmailService emailService)
+        public ServicoInterno(ILogger<ServicoInterno> logger, IEmailService emailService, IConfiguration config, PdsContext context)
         {
             _logger = logger;
             _emailService = emailService;
+            _config = config;
+            _context = context;
         }
 
-        public void Executar()
+        public async Task Executar()
         {
             // Lógica da tarefa interna
-           //var aluguers = _();
+            var aluguers = await _context.Aluguers
+                .Include(a=>a.ClienteIdclienteNavigation)
+                .Where(a=>a.EstadoAluguer=="Aguada Levantamento" || a.EstadoAluguer == "Alugado")
+                .ToListAsync();
 
-            _logger.LogInformation("Executando tarefa diária às {hora}", DateTime.Now);
+
+            foreach (var aluguer in aluguers)
+            {
+                if (aluguer.DataDevolucao < DateTime.Now && aluguer.EstadoAluguer == "Alugado")
+                {
+                    var cliente = aluguer.ClienteIdclienteNavigation;
+                    if (cliente != null )
+                    {
+                        var email = cliente.LoginIdloginNavigation.Email;
+                        var assunto = "Notificação de Infração - Resolução da sua contestacao";
+                        var mensagem = $"Caro/a {cliente.NomeCliente},<br><br>Vimos por este meio informá-lo(a) de que, a data de devolução <br><br>" +
+                                        $"<br><br><br>__<br>" +
+                                        $"Com os melhores cumprimentos,<br>" +
+                                        $"&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<b><i>CarXpress Team</i></b><br><br>" +
+                                        $"&emsp;<b>Empresa:</b>&emsp;&emsp;&emsp;  CarExpress, Lda<br>" +
+                                        $"&emsp;<b>Contacto:</b>&emsp;&emsp;&emsp;  963 183 446<br>" +
+                                        $"&emsp;<b>Morada:</b>&emsp;&emsp;&emsp;&emsp;Rua das Ameixas, Nº54, 1234-567, Frossos, Braga";
+                        await _emailService.EnviarEmail(email, assunto, mensagem);
+
+                    }
+                }
+
+                if (aluguer.DataLevantamento < DateTime.Now && aluguer.EstadoAluguer == "Aguada Levantamento")
+                {
+                    
+                }
+            }
+
+
+                _logger.LogInformation("Executando tarefa diária às {hora}", DateTime.Now);
             _logger.LogInformation("BANANA");
 
         }
